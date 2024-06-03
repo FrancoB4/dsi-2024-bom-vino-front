adb14faef2f9457a1475de70f7c39913b07eb437
FrancoB4 <francobonfigliovazquez@gmail.com>
2024-06-03 18:02:52 -0300

    Se agrega consulta a la api en lista ranking

--- src/ListaRank.js
+++ src/ListaRank.js
@@ -3,64 +3,66 @@ import Vino from './Vino';
 import wine1 from './image/carnivor-wine.png'
 import wine2 from './image/sutterhome-wine.png'
 
-const vinos = [
-    {
-        nombre: "Cabernet",
-        image: wine1,
-        bodega: "Carnivor",
-        precio: "$10000",
-        rating: 3.8,
-    },
-    {
-        nombre: "White Zinfadel",
-        image: wine2,
-        bodega: "Sutter Home",
-        precio: "$40000",
-        rating: 4.5,
-    },
-    {
-        nombre: "Cabernet",
-        image: wine1,
-        bodega: "Carnivor",
-        precio: "$10000",
-        rating: 3.8,
-    },
-    {
-        nombre: "White Zinfadel",
-        image: wine2,
-        bodega: "Sutter Home",
-        precio: "$40000",
-        rating: 4.5,
-    },
-    {
-        nombre: "Cabernet",
-        image: wine1,
-        bodega: "Carnivor",
-        precio: "$10000",
-        rating: 3.8,
-    },
-    {
-        nombre: "White Zinfadel",
-        image: wine2,
-        bodega: "Sutter Home",
-        precio: "$40000",
-        rating: 4.5,
-    },
-    {
-        nombre: "Cabernet",
-        image: wine1,
-        bodega: "Carnivor",
-        precio: "$10000",
-        rating: 3.8,
-    },
-    {
-        nombre: "White Zinfadel",
-        image: wine2,
-        bodega: "Sutter Home",
-        precio: "$40000",
-        rating: 4.5,
-    },
-]
+const res = await fetch('http://localhost:8080/api/v1/models/vinos');
+const vinos = await res.json();
+// const vinos = [
+//     {
+//         nombre: "Cabernet",
+//         image: wine1,
+//         bodega: "Carnivor",
+//         precio: "$10000",
+//         rating: 3.8,
+//     },
+//     {
+//         nombre: "White Zinfadel",
+//         image: wine2,
+//         bodega: "Sutter Home",
+//         precio: "$40000",
+//         rating: 4.5,
+//     },
+//     {
+//         nombre: "Cabernet",
+//         image: wine1,
+//         bodega: "Carnivor",
+//         precio: "$10000",
+//         rating: 3.8,
+//     },
+//     {
+//         nombre: "White Zinfadel",
+//         image: wine2,
+//         bodega: "Sutter Home",
+//         precio: "$40000",
+//         rating: 4.5,
+//     },
+//     {
+//         nombre: "Cabernet",
+//         image: wine1,
+//         bodega: "Carnivor",
+//         precio: "$10000",
+//         rating: 3.8,
+//     },
+//     {
+//         nombre: "White Zinfadel",
+//         image: wine2,
+//         bodega: "Sutter Home",
+//         precio: "$40000",
+//         rating: 4.5,
+//     },
+//     {
+//         nombre: "Cabernet",
+//         image: wine1,
+//         bodega: "Carnivor",
+//         precio: "$10000",
+//         rating: 3.8,
+//     },
+//     {
+//         nombre: "White Zinfadel",
+//         image: wine2,
+//         bodega: "Sutter Home",
+//         precio: "$40000",
+//         rating: 4.5,
+//     },
+// ]
 
 function ListaRank() {
     return (
